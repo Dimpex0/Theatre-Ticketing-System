485c17e3c5a080b81bba81c1beec19a3255104b0
HP 830 G5 <mitkodd2005@gmail.com>
2026-03-26 19:48:20 +0200

    Base structure

---
+++ theatre-managing-system/src/main/java/com/fmi/webdevelopment/theatremanagingsystem/TheatreManagingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fmi.webdevelopment.theatremanagingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheatreManagingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheatreManagingSystemApplication.class, args);
+    }
+
+}
---
+++ theatre-managing-system/src/test/java/com/fmi/webdevelopment/theatremanagingsystem/TheatreManagingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmi.webdevelopment.theatremanagingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheatreManagingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
